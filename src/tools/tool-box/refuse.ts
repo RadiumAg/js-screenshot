a1c2e37577ca33d7625a2502e62bd78544c6ab4f
Radium <2043869032@qq.com>
2023-10-04 21:27:58 +0800

    chore: 增加关闭功能

--- src/tools/tool-box/refuse.ts
+++ src/tools/tool-box/refuse.ts
@@ -1,3 +1,5 @@
+import Style from '@screenshots/theme/refuse.module.scss';
+import refuse from '@screenshots/assets/images/refuse.svg?raw';
 import BaseBox from '../baseBox';
 import CutoutBox from '../cutout-box/cutoutBox';
 
@@ -5,4 +7,26 @@ class Refuse extends BaseBox {
   constructor(private cutoutBox: CutoutBox) {
     super();
   }
+
+  el: HTMLDivElement | null = null;
+
+  updatePosition(): void {
+    /** empty **/
+  }
+
+  protected initEvent(): void {
+    this.el?.addEventListener('click', () => {
+      this.cutoutBox.destroy();
+    });
+  }
+
+  initRefuse() {
+    this.el = document.createElement('div');
+    this.el.classList.add(Style.refuse);
+    this.el.innerHTML = refuse;
+
+    this.initEvent();
+  }
 }
+
+export default Refuse;
