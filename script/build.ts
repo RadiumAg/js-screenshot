4f9ca9d5c7f3e4106c9eb8ce406981491a096853
Radium <2043869032@qq.com>
2025-02-25 16:55:36 +0800

    build: update deployment workflow and build scripts

--- script/build.ts
+++ script/build.ts
@@ -1,78 +1,93 @@
-import {resolve} from 'path';
-import {watch} from 'rollup';
+import { resolve } from 'path';
 import alias from '@rollup/plugin-alias';
 import image from '@rollup/plugin-image';
 import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
 import replace from '@rollup/plugin-replace';
+import { RollupOptions, RollupWatchOptions, rollup, watch } from 'rollup';
+import { getArgs } from './util';
+
+const buildConfig = {
+  input: resolve(__dirname, '../src/screenShot.ts'),
+  plugins: [
+    alias({
+      entries: [
+        { find: '@screenshots', replacement: resolve(__dirname, '../src') },
+      ],
+    }),
+    image({ dom: true }),
+    postcss({
+      modules: true,
+      extract: true,
+      sourceMap: false,
+      namedExports(id) {
+        const nameArray = id.split('-');
+        const name = nameArray.reduce((pre, current, index) => {
+          if (index === 0) {
+            return pre;
+          } else {
+            return pre + current[0].toUpperCase() + current.slice(1);
+          }
+        });
+
+        return name;
+      },
+    }),
+    typescript({
+      compilerOptions: {
+        target: 'esnext',
+        module: 'esnext',
+        strict: true,
+        skipLibCheck: true,
+        declaration: true,
+        rootDir: resolve(__dirname, '../src'),
+        declarationDir: resolve(__dirname, '../dist/esm'),
+      },
+      include: [
+        resolve(__dirname, './type.d.ts'),
+        resolve(__dirname, '../src/**/*.ts'),
+      ],
+      exclude: ['node_modules'],
+    }),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+  external: ['html2canvas'],
+  output: [
+    {
+      format: 'esm',
+      dir: resolve(__dirname, '../dist/esm'),
+      globals: { html2canvas: 'html2canvas' },
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+    },
+  ],
+};
 
 async function build() {
-  console.log(process.env.NODE_ENV);
-  const watcher = watch({
-    input: resolve(__dirname, '../src/screenShot.ts'),
-    plugins: [
-      alias({
-        entries: [{find: '@screenshots', replacement: resolve(__dirname, '../src')}],
-      }),
-      image({dom: true}),
-      postcss({
-        modules: true,
-        extract: true,
-        sourceMap: false,
-        namedExports(id) {
-          const nameArray = id.split('-');
-          const name = nameArray.reduce((pre, current, index) => {
-            if (index === 0) {
-              return pre;
-            } else {
-              return pre + current[0].toUpperCase() + current.slice(1);
-            }
-          });
+  const args = getArgs();
 
-          return name;
-        },
-      }),
-      typescript({
-        compilerOptions: {
-          target: 'esnext',
-          module: 'esnext',
-          strict: true,
-          skipLibCheck: true,
-          declaration: true,
-          rootDir: resolve(__dirname, '../src'),
-          declarationDir: resolve(__dirname, '../dist/esm'),
-        },
-        include: [resolve(__dirname, './type.d.ts'), resolve(__dirname, '../src/**/*.ts')],
-        exclude: ['node_modules'],
-      }),
-      replace({
-        preventAssignment: true,
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      }),
-    ],
-    external: ['html2canvas'],
-    output: [
-      {
-        format: 'esm',
-        dir: resolve(__dirname, '../dist/esm'),
-        globals: {html2canvas: 'html2canvas'},
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
-    ],
-  });
+  if (Reflect.has(args, '--watch')) {
+    const watcher = watch(
+      buildConfig as RollupWatchOptions | RollupWatchOptions[],
+    );
 
-  watcher.on('event', (event) => {
-    if (event.code === 'START') {
-      console.log('📦 build start...');
-    } else if (event.code === 'BUNDLE_END') {
-      console.log('📦 build end');
-    } else if (event.code === 'ERROR') {
-      console.error('📦 build error:', event.error);
-    } else if (event.code === 'END') {
-      console.log('📦 build end');
-    }
-  });
+    watcher.on('event', event => {
+      if (event.code === 'START') {
+        console.log('📦 build start...');
+      } else if (event.code === 'BUNDLE_END') {
+        console.log('📦 build end');
+      } else if (event.code === 'ERROR') {
+        console.error('📦 build error:', event.error);
+      } else if (event.code === 'END') {
+        console.log('📦 build end');
+      }
+    });
+  } else {
+    rollup(buildConfig as RollupOptions);
+  }
 }
 
 build();
---
+++ script/util.ts
@@ -0,0 +1,16 @@
+const getArgs = () => {
+  const args = process.argv.slice(2);
+  const parsedArgs: Record<string, any> = {};
+
+  // 遍历参数数组，解析键值对
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i];
+    const value = args[i + 1];
+    parsedArgs[key] = value;
+  }
+
+  console.log(parsedArgs);
+  return parsedArgs;
+};
+
+export { getArgs };
