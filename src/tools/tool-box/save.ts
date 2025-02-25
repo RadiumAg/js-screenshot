6eae3d2be5a6066e19c44e248e4590f2c2e3d904
Radium <2043869032@qq.com>
2025-02-25 16:18:17 +0800

    refactor: format code in save.ts

--- src/tools/tool-box/save.ts
+++ src/tools/tool-box/save.ts
@@ -1,6 +1,6 @@
 import Style from '@screenshots/theme/save.module.scss';
 import save from '@screenshots/assets/images/save.svg';
-import {__isDev__, useDownLoad} from '@screenshots/utils';
+import { __isDev__, useDownLoad } from '@screenshots/utils';
 import BaseBox from '../baseBox';
 import CutoutBox from '../cutout-box/cutoutBox';
 
@@ -17,7 +17,12 @@ class Save extends BaseBox {
     const download = useDownLoad();
 
     this.el?.addEventListener('click', () => {
-      const screenShotData = this.context.getImageData(this.cutoutBox.x, this.cutoutBox.y, this.cutoutBox.width, this.cutoutBox.height);
+      const screenShotData = this.context.getImageData(
+        this.cutoutBox.x,
+        this.cutoutBox.y,
+        this.cutoutBox.width,
+        this.cutoutBox.height,
+      );
 
       const screenCanvas = document.createElement('canvas');
       screenCanvas.width = this.cutoutBox.width;
@@ -25,17 +30,22 @@ class Save extends BaseBox {
       screenCanvas.getContext('2d')?.putImageData(screenShotData, 0, 0);
 
       if (__isDev__) {
-        console.info('screen width', this.cutoutBox.width, 'screen height', this.cutoutBox.height);
+        console.info(
+          'screen width',
+          this.cutoutBox.width,
+          'screen height',
+          this.cutoutBox.height,
+        );
       }
 
       screenCanvas.toBlob(
-        (blob) => {
+        blob => {
           if (!blob) return;
           const url = URL.createObjectURL(blob);
           download('截图', url);
         },
         'image/png',
-        1
+        1,
       );
     });
   }
@@ -48,7 +58,11 @@ class Save extends BaseBox {
     this.initEvent();
   }
 
-  destory(): void {}
+  destory(): void {
+    /**
+     * empty
+     */
+  }
 }
 
 export default Save;
