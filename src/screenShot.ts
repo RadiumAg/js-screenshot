84229631853a7c7c7cdaebe2598ecb3151389034
Radium <2043869032@qq.com>
2025-02-25 14:51:43 +0800

    feat: enhance screenshot functionality with improved media display handling and options logging

--- src/screenShot.ts
+++ src/screenShot.ts
@@ -8,6 +8,10 @@ import {
 } from './tools/canvas';
 import CutoutBox from './tools/cutout-box/cutoutBox';
 
+type ScreenShotOptions = {
+  afterFinished: () => void;
+};
+
 function createCanvas(width: number, height: number) {
   const canvas = document.createElement('canvas');
 
@@ -22,35 +26,37 @@ function createCanvas(width: number, height: number) {
   return canvas;
 }
 
+// 物理像素/ css = 屏幕 / window.innerHeight
 async function displayMediaMode() {
   let resolveFn: (value: unknown) => void;
-
   const promise = new Promise(resolve => {
     resolveFn = resolve;
   });
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
   const captureStream = await navigator.mediaDevices.getDisplayMedia({
     video: {
       displaySurface: 'window',
     },
   });
+  setSourceCanvasElement(createCanvas(width, height));
+  setCanvasElement(createCanvas(width, height));
 
-  const { width, height } = captureStream.getVideoTracks()[0].getSettings();
-
-  setSourceCanvasElement(createCanvas(width || 0, height || 0));
-  setCanvasElement(createCanvas(window.innerWidth, window.innerHeight));
   setVideoElement(document.createElement('video'));
-
+  videoElement.width = width;
+  videoElement.height = height;
+  videoElement.style.objectFit = 'cover';
   videoElement.srcObject = captureStream;
 
   videoElement.addEventListener('loadedmetadata', () => {
     videoElement.play();
-
     const sourceContext = sourceCanvasElement.getContext('2d');
 
     setTimeout(() => {
-      sourceContext?.drawImage(videoElement, 0, 0);
+      sourceContext?.drawImage(videoElement, 0, 0, 0, 0, 0, 0, width, height);
       document.body.append(canvasElement);
+      document.body.append(videoElement);
 
       resolveFn('init');
     }, 1000);
@@ -60,7 +66,9 @@ async function displayMediaMode() {
 }
 
 class ScreenShot {
-  constructor(private screenShotOptions: ScreenShotOptions) {}
+  constructor(private screenShotOptions: ScreenShotOptions) {
+    console.log('options', this.screenShotOptions);
+  }
 
   async shot() {
     await displayMediaMode();
