049d59120396ec7a16466060e654c595b5823e1c
zly <zly01858966@alibaba-inc.com>
2026-05-22 14:00:43 +0800

    refactor: remove unused event listeners hook and exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
-export type { EventListenerMap } from '../hooks/use-event-listeners';
 export type { Position, PositionState, Size } from '../hooks/use-position';
 export { createAndRenderComponent, destroyComponentContainer, getComponentElement, renderComponent } from './utils/render-utils';
